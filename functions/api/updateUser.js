0b821c471f11439d90fc8056fd5750ec1b46fd26
codeur-asm-974 <eric974.sermande@gmail.com>
2024-03-24 16:43:48 +0400

    modification

--- functions/api/updateUser.js
+++ functions/api/updateUser.js
@@ -2,20 +2,13 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors")({ origin: true });
 
-/**
- *Update user identification data
- *@param body {String,Object}
- *@return {string} message success|error
- *@forplay https://us-central1-courreact.cloudfunctions.net/updateUser
- *@documentation https://firebase.google.com/docs/auth/admin/manage-users?hl=fr&authuser=0
- */
 exports.updateUser = functions.https.onRequest((req, res) => {
   const uid = req.body.uid;
   const data = req.body.data;
 
   return cors(req, res, async () => {
     try {
-      const userRecord = await admin.auth.updateUser(uid, data);
+      const userRecord = await admin.auth().updateUser(uid, data);
       res.send(userRecord.toJSON());
     } catch (error) {
       res.status(500).send(error);
